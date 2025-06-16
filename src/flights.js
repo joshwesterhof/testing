8729de43e59764c1b7ab9dd74b93a7461c4942e7
joshwesterhof <josh@joshwesterhof.com>
2025-06-16 11:13:53 -0800

    8

---
+++ src/flights.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    airline: "Delta",
+    flight: "DL 123",
+    from: "JFK",
+    to: "LAX",
+    time: "12:30 PM",
+    status: "On Time",
+    type: "Departure"
+  },
+  {
+    airline: "Alaska",
+    flight: "AS 456",
+    from: "SEA",
+    to: "ANC",
+    time: "1:15 PM",
+    status: "Delayed",
+    type: "Arrival"
+  }
+];
